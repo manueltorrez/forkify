2930e1f53da05507b85bcbafe20a77b5d9fbe8b7
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-11-03 10:42:09 -0600

    Testing food2fork API with axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
 // Global app controller
-const x = "culo peludo";
-console.log(`My wifi network name is ${x}`);
+import axios from 'axios';
+
+async function getResult(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'f1863630e56dfed8f801b7fb82562b3a';
+
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+
+}
+
+getResult('pizza');
