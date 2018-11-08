01db4832077f7f170c2ae891ce05b791a544c62b
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-11-08 07:56:25 -0600

    Building the Shopping List Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -120,5 +121,7 @@ elements.recipe.addEventListener('click', e => {
         recipeView.updateServingsIngredients(state.recipe);
     }
 
-    console.log(state.recipe);
 });
+
+
+window.l = new List();
